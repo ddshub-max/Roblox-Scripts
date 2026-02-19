19d79315e071acb5bcfb0b20fd27b01ad43363f7
ddshub-max <maulaababil06@gmail.com>
2026-02-20 03:42:24 +0700

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,5 +1,5 @@
 -- Simpan ini di GitHub Raw atau Pastebin Raw
 return {
-    [10181783134] = true, -- UserID Kamu
+    [10181783134] = false, -- UserID Kamu
     [1234567890] = true,  -- Contoh ID lain
 }
